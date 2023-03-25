1823821a292361a4a53994ea324ce8dce7dc1803
wdtkr <wdtkru@icloud.com>
2023-03-26 03:54:34 +0900

    リポジトリ移動

---
+++ Assets/Scripts/WebRtcScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Unity.WebRTC;
+
+public class WebRtcScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
